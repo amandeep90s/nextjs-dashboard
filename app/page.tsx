d5762e5cfc82477b61c53a49ccc156ab475ff1ac
Amandeep Singh <programmeramandeep@gmail.com>
2025-03-23 19:21:04 +0530

    chore: Remove bycrypt and add bcryptjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { lusitana } from "@/app/ui/fonts";
-import styles from "@/app/ui/home.module.css";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
@@ -13,8 +12,7 @@ export default function Page() {
       </div>
       <div className="flex flex-col gap-4 mt-4 grow md:flex-row">
         <div className="flex flex-col justify-center gap-6 px-6 py-10 rounded-lg bg-gray-50 md:w-2/5 md:px-20">
-          {/* <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" /> */}
-          <div className={styles.shape}></div>
+          <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
