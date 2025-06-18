c0fdd4f8460d4df881763740cb592b500556cb0c
Amandeep Singh <programmeramandeep@gmail.com>
2025-06-19 00:12:57 +0530

    chore: Updated dependencies

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,14 @@
+import { Card } from "@/app/ui/dashboard/cards";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import { lusitana } from "@/app/ui/fonts";
+
 export default function Dashboard() {
-  return <p>Dashboard page</p>;
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+    </main>
+  );
 }
